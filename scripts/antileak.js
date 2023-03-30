875252072b655557e3283a5b7fcac9e88de62f70
96DarkCode96 <76725766+96DarkCode96@users.noreply.github.com>
2023-03-30 21:17:56 +0200

    Delete scripts directory

--- scripts/antileak.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("contextmenu", (e) => {
-    //e.preventDefault();
-}, false);
-document.addEventListener("keydown", (e) => {
-	if (e.ctrlKey || e.keyCode==123) {
-        e.stopPropagation();
-        e.preventDefault();
-	}
-});
